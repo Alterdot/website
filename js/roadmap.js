181106a91361b193b19f3e896571cd2ab78ebe22
Andrew Tookay <andrewtookay@gmail.com>
2021-10-17 16:58:43 +0300

    added Exbitron exchange and fixed roadmap traversal

--- js/roadmap.js
+++ js/roadmap.js
@@ -1,103 +1,109 @@
 (function () {
 
-  // VARIABLES
-  const timeline = document.querySelector(".timeline"),
-  arrows = document.querySelectorAll("#roadmap .arrows .arrow"),
-  arrowPrev = document.querySelector("#roadmap .arrows .arrow__prev"),
-  arrowNext = document.querySelector("#roadmap .arrows .arrow__next"),
-  firstItem = document.querySelector("#roadmap p:first-child"),
-  lastItem = document.querySelector("#roadmap p:last-child"),
-  xScrolling = 840,
-  disabledClass = "disabled";
+    // VARIABLES
+    const timeline = document.querySelector(".timeline"),
+    arrows = document.querySelectorAll("#roadmap .arrows .arrow"),
+    arrowPrev = document.querySelector("#roadmap .arrows .arrow__prev"),
+    arrowNext = document.querySelector("#roadmap .arrows .arrow__next"),
+    firstItem = document.querySelector("#roadmap p:first-child"),
+    lastItem = document.querySelector("#roadmap p:last-child"),
+    xScrolling = 840,
+    disabledClass = "disabled";
 
-  // START
-  window.addEventListener("load", init);
+    // START
+    window.addEventListener("load", init);
 
-  function init() {
-    animateTl(xScrolling, arrows, timeline);
-    setSwipeFn(timeline, arrowPrev, arrowNext);
-    setKeyboardFn(arrowPrev, arrowNext);
-  }
-
-  // CHECK IF AN ELEMENT IS IN VIEWPORT
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth));
-
-  }
-
-  // SET STATE OF PREV/NEXT ARROWS
-  function setBtnState(el, flag = true) {
-    if (flag) {
-      el.classList.add(disabledClass);
-    } else {
-      if (el.classList.contains(disabledClass)) {
-        el.classList.remove(disabledClass);
-      }
-      el.disabled = false;
+    function init() {
+        animateTl(xScrolling, arrows, timeline);
+        setSwipeFn(timeline, arrowPrev, arrowNext);
+        setKeyboardFn(arrowPrev, arrowNext);
     }
-  }
 
-  // ANIMATE TIMELINE
-  function animateTl(scrolling, el, tl) {
-    let counter = 0;
-    for (let i = 0; i < el.length; i++) {
-      el[i].addEventListener("click", function () {
-        if (!arrowPrev.disabled) {
-          arrowPrev.disabled = true;
-        }
-        if (!arrowNext.disabled) {
-          arrowNext.disabled = true;
-        }
-        const sign = this.classList.contains("arrow__prev") ? "" : "-";
-        if (counter === 0) {
-          tl.style.transform = `translateX(-${scrolling}px)`;
+    // CHECK IF AN ELEMENT IS IN VIEWPORT
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // SET STATE OF PREV/NEXT ARROWS
+    function setBtnState(el, flag = true) {
+        if (flag) {
+            el.classList.add(disabledClass);
         } else {
-          const tlStyle = getComputedStyle(tl);
-          // add more browser prefixes if needed here
-          const tlTransform = tlStyle.getPropertyValue("-webkit-transform") || tlStyle.getPropertyValue("transform");
-          const values = parseInt(tlTransform.split(",")[4]) + parseInt(`${sign}${scrolling}`);
-          tl.style.transform = `translateX(${values}px)`;
+            if (el.classList.contains(disabledClass)) {
+            el.classList.remove(disabledClass);
+            }
+            el.disabled = false;
         }
-
-        setTimeout(() => {
-          isElementInViewport(firstItem) ? setBtnState(arrowPrev) : setBtnState(arrowPrev, false);
-          isElementInViewport(lastItem) ? setBtnState(arrowNext) : setBtnState(arrowNext, false);
-        }, 250);
-
-        counter++;
-      });
     }
-  }
 
-  // ADD SWIPE SUPPORT FOR TOUCH DEVICES
-  function setSwipeFn(tl, prev, next) {
-    const hammer = new Hammer(tl);
-    hammer.on("swipeleft", () => next.click());
-    hammer.on("swiperight", () => prev.click());
-  }
+    // ANIMATE TIMELINE
+    function animateTl(scrolling, el, tl) {
+        let counter = 0;
 
-  // ADD BASIC KEYBOARD FUNCTIONALITY
-  function setKeyboardFn(prev, next) {
-    document.addEventListener("keydown", e => {
-      if (e.which === 37 || e.which === 39) {
-        const timelineOfTop = timeline.offsetTop;
-        const y = window.pageYOffset;
-        if (timelineOfTop !== y) {
-          window.scrollTo(0, timelineOfTop);
+        for (let i = 0; i < el.length; i++) {
+            el[i].addEventListener("click", function () {
+            if (!arrowPrev.disabled) {
+                arrowPrev.disabled = true;
+            }
+            if (!arrowNext.disabled) {
+                arrowNext.disabled = true;
+            }
+            const sign = this.classList.contains("arrow__prev") ? "" : "-";
+            if (counter === 0) {
+                tl.style.transform = `translateX(-${scrolling}px)`;
+            } else {
+                const tlStyle = getComputedStyle(tl);
+                // add more browser prefixes if needed here
+                const tlTransform = tlStyle.getPropertyValue("-webkit-transform") || tlStyle.getPropertyValue("transform");
+                const values = parseInt(tlTransform.split(",")[4]) + parseInt(`${sign}${scrolling}`);
+                tl.style.transform = `translateX(${values}px)`;
+            }
+
+            setTimeout(() => {
+                isElementInViewport(firstItem) ? setBtnState(arrowPrev) : setBtnState(arrowPrev, false);
+                isElementInViewport(lastItem) ? setBtnState(arrowNext) : setBtnState(arrowNext, false);
+            }, 500); // the animation takes 0.4s so this has to take into account any other delays, floating point width offsets appear if not given enough timeout
+
+            counter++;
+            });
         }
-        if (e.which === 37) {
-          prev.click();
-        } else if (e.which === 39) {
-          next.click();
-        }
-      }
-    });
-  }
+    }
+
+    // ADD SWIPE SUPPORT FOR TOUCH DEVICES
+    function setSwipeFn(tl, prev, next) {
+        const hammer = new Hammer(tl);
+        hammer.on("swipeleft", () => next.click());
+        hammer.on("swiperight", () => prev.click());
+    }
+
+    // ADD BASIC KEYBOARD FUNCTIONALITY
+    function setKeyboardFn(prev, next) {
+        document.addEventListener("keydown", e => {
+            if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+                const timelineOffTop = timeline.offsetTop;
+                const timelineOffHeight = timeline.offsetHeight;
+                const y = window.pageYOffset;
+                const height = window.innerHeight;
+
+                if (timelineOffTop < y || timelineOffTop + timelineOffHeight > y + height) {
+                    return;
+                }
+
+                if (e.key === "ArrowLeft") {
+                    prev.click();
+                } else if (e.key === "ArrowRight") {
+                    next.click();
+                }
+            }
+        });
+    }
 
 })();
